7963964d143ee3fc5ec4fe9f633abde97b83b471
Corey Holland <czwolf@gmail.com>
2026-06-14 07:23:36 +0000

    fix(ble): add debugName entries for all subscribed characteristics

--- app/src/main/kotlin/com/zwheel/app/ble/BleFormatExtensions.kt
+++ app/src/main/kotlin/com/zwheel/app/ble/BleFormatExtensions.kt
@@ -9,14 +9,25 @@ internal fun ScanResult.deviceKey(): String = deviceId.lowercase()
 internal fun GattCharacteristicId.debugName(): String =
     when (this) {
         OwUuids.BATTERY_PERCENT -> "battery_percent"
+        OwUuids.BATTERY_TEMPERATURE -> "battery_temperature"
         OwUuids.RPM -> "rpm"
         OwUuids.PACK_VOLTAGE -> "pack_voltage"
+        OwUuids.CELL_VOLTAGES -> "cell_voltages"
         OwUuids.AMPS -> "amps"
-        OwUuids.TEMPERATURE -> "temperature"
+        OwUuids.TRIP_TOTAL_AMP_HOURS -> "trip_amp_hours"
+        OwUuids.TRIP_REGEN_AMP_HOURS -> "trip_regen_amp_hours"
+        OwUuids.TEMPERATURE -> "controller_motor_temp"
+        OwUuids.SAFETY_HEADROOM -> "safety_headroom"
+        OwUuids.STATUS_ERROR -> "status_error"
+        OwUuids.PITCH -> "pitch"
+        OwUuids.ROLL -> "roll"
+        OwUuids.YAW -> "yaw"
         OwUuids.RIDE_MODE -> "ride_mode"
+        OwUuids.ODOMETER -> "odometer"
         OwUuids.HARDWARE_REVISION -> "hardware_revision"
         OwUuids.FIRMWARE_REVISION -> "firmware_revision"
         OwUuids.UART_WRITE -> "uart_write"
+        OwUuids.UART_READ -> "uart_read"
         else -> uuid.toString().substring(startIndex = 4, endIndex = 8)
     }
 

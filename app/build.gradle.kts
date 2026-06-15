aa104c379308110ad66d08b388fe923759605421
Corey Zev <czwolf@gmail.com>
2026-06-15 01:29:36 -0400

    build(wear): drop embedded wearApp() delivery; document adb + Play install paths (#90)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -121,7 +121,6 @@ dependencies {
     implementation(libs.osmdroid)
     implementation(libs.play.services.location)
     implementation(libs.play.services.wearable)
-    wearApp(project(":wear"))
     implementation(libs.kotlinx.coroutines.android)
     implementation(libs.kotlinx.coroutines.core)
     testImplementation(libs.androidx.test.core)
